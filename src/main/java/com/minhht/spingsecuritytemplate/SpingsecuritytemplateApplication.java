33a2879204195ffaac3af96fb5d76066fcfd0f97
Minh Hoang <minh.ht173259@gmail.com>
2024-12-15 01:02:54 +0700

    initial project

---
+++ src/main/java/com/minhht/spingsecuritytemplate/SpingsecuritytemplateApplication.java
@@ -0,0 +1,12 @@
+package com.minhht.spingsecuritytemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingsecuritytemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpingsecuritytemplateApplication.class, args);
+  }
+}
---
+++ src/test/java/com/minhht/spingsecuritytemplate/SpingsecuritytemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhht.spingsecuritytemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingsecuritytemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
